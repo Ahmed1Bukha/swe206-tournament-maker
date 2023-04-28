ccaa842165804c86fc9e1e1301c1d16b0f327047
Ahmed_Bukha <xxbu7aamedxx@gmail.com>
2023-04-28 20:30:42 +0300

    Update create_elimination.dart

--- frontEnd/swe206/lib/home_admin/create_types_pages/create_elimination.dart
+++ frontEnd/swe206/lib/home_admin/create_types_pages/create_elimination.dart
@@ -1,55 +1,239 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class CreateEleminationPage extends StatelessWidget {
+enum BasedTeam { single, team }
+
+class CreateEleminationPage extends StatefulWidget {
   const CreateEleminationPage({super.key});
 
+  @override
+  State<CreateEleminationPage> createState() => _CreateEleminationPageState();
+}
+
+class _CreateEleminationPageState extends State<CreateEleminationPage> {
+  BasedTeam? _character = BasedTeam.single;
+  String dropdownvalue = 'VolleyBall';
+  var games = [
+    'VolleyBall',
+    'FootBall',
+    'Boxing',
+    'BasketBall',
+  ];
+
+  final myTournamentController = TextEditingController();
+  final myNumberController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Create elimination tourmanent"),
+        title: const Text(
+          "Create elimination tourmanent",
+        ),
       ),
-      body: Column(
-        children: [listDropDown()],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Tournament Name: ",
+                style: TextStyle(fontSize: 20),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                controller: myTournamentController,
+                decoration: const InputDecoration(
+                  hintText: "Enter tournament name",
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                "Pick a game:",
+                style: TextStyle(fontSize: 20),
+              ),
+              Center(
+                child: getDropDownButton(),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () => _showMyDialog(),
+                  child: Text("Add a game"),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                "Pick type of participants: ",
+                style: TextStyle(fontSize: 20),
+              ),
+              Column(
+                children: <Widget>[
+                  ListTile(
+                    title: const Text('single'),
+                    leading: Radio<BasedTeam>(
+                      value: BasedTeam.single,
+                      groupValue: _character,
+                      onChanged: (BasedTeam? value) {
+                        setState(() {
+                          _character = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('team'),
+                    leading: Radio<BasedTeam>(
+                      value: BasedTeam.team,
+                      groupValue: _character,
+                      onChanged: (BasedTeam? value) {
+                        setState(() {
+                          _character = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                "Enter number of participant:",
+                style: TextStyle(fontSize: 20),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some number';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+                controller: myNumberController,
+                decoration: const InputDecoration(
+                  hintText: "Enter number",
+                ),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Center(
+                  child: ElevatedButton(
+                onPressed: () {
+                  //TODO connect to backend to add the stuff.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    print(myTournamentController.text);
+                    print(dropdownvalue);
+                    print(myNumberController.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      // we can use this as await.
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                    Navigator.pop(context);
+                  }
+                },
+                child: const Text(
+                  "Submit",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ))
+            ],
+          ),
+        ),
       ),
     );
   }
-}
 
-class listDropDown extends StatefulWidget {
-  listDropDown({super.key});
-  List<String> list = <String>['One', 'Two', 'Three', 'Four'];
-  @override
-  State<listDropDown> createState() => _listDropDownState();
-}
+  DropdownButton<String> getDropDownButton() {
+    return DropdownButton(
+      // Initial Value
+      value: dropdownvalue,
+      hint: const Text("Pick a game"),
 
-class _listDropDownState extends State<listDropDown> {
-  @override
-  Widget build(BuildContext context) {
-    String dropdownValue = widget.list.first;
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String? value) {
-        // This is called when the user selects an item.
-        setState(() {
-          dropdownValue = value!;
-        });
-      },
-      items: widget.list.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
+      // Down Arrow Icon
+      icon: const Icon(Icons.keyboard_arrow_down),
+
+      // Array list of items
+      items: games.map((String items) {
+        return DropdownMenuItem(
+          value: items,
+          child: Text(items),
         );
       }).toList(),
+      // After selecting the desired option,it will
+      // change button value to selected value
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownvalue = newValue!;
+        });
+      },
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    final _formKey = GlobalKey<FormState>();
+    final gameNameController = TextEditingController();
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add game'),
+          content: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: ListBody(
+                children: <Widget>[
+                  const Text('Enter game name:'),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    controller: gameNameController,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Add'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  games.add(gameNameController.text);
+                  Navigator.of(context).pop();
+                  setState(() {});
+                }
+              },
+            ),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
